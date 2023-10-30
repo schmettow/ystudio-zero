48548ab074d84807431025cd0e024b2c30551202
schmettow <schmettow@web.de>
2023-10-30 07:59:32 +0100

    more comments

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,18 @@ mod ystudio;
 use ystudio::*;
 
 fn main() {
+    // creating a bi-directional channel.
+    // the commander sends commands to the YLab thread
+    // the listener receives data from the YLab thread
     let (ylab_cmd, ylab_listen) 
         = mpsc::channel();
+    // The ystudio object contains three coponents in a thread safe manner
+    // + ylab_state, which is a mutexed YLabState
+    // + ylab_data, which is a egui History of YLab Samples
+    // + ui, which captures UI related variables
     let ystudio = ystudio::Ystudio::new(ylab_cmd);
-    // starting the serial thread, 
-    // consuming all mutexes
+    // The thread to collect Ylab data is started
+    // consuming copies of ylab state, data and command listener
     thread::spawn(move || {
         threads::ylab_thread(
             ystudio.ylab_state,
@@ -18,6 +25,8 @@ fn main() {
         );
     });
 
-    // starting the egui
+    // starting the egui, consuming the ystudio object.
+    // The details of the GUI are in gui.rs.
+    // The below works, because Ystudio objects implement eframe::App.
     gui::egui_init(ystudio);
 }
