c345273fe5eb710677e53d398ec29e828f144cdc
schmettow <schmettow@web.de>
2023-11-02 19:41:31 +0100

    + ylab_data is transferred as Yld (not Ytf8) + ystudio_time now in seconds

--- src/threads.rs
+++ src/threads.rs
@@ -16,7 +16,7 @@ use egui::emath::History;
 /// 
 pub fn ylab_thread(
     ylab_state: Arc<Mutex<State>>,
-    ylab_data: Arc<Mutex<History<Ytf8>>>,
+    ylab_data: Arc<Mutex<History<Yld>>>,
     ylab_listen: mpsc::Receiver<YLabCmd>,
     ) -> ! {
     
@@ -36,10 +36,10 @@ pub fn ylab_thread(
                     // In the previous state we've checked that the port works, so we can unwrap
                     let port 
                         = serialport::new(port, version.baud() as u32)
-                                        .timeout(Duration::from_millis(1))
-                                        .flow_control(serialport::FlowControl::Software)
-                                        .open()
-                                        .unwrap();
+                            .timeout(Duration::from_millis(1))
+                            .flow_control(serialport::FlowControl::Software)
+                            .open()
+                            .unwrap();
                     let reader = BufReader::new(port);
                     
                     for line in reader.lines() {
@@ -51,18 +51,25 @@ pub fn ylab_thread(
                         let possible_sample 
                             = Ytf8::from_csv_line(&line);
                         // print a dot when sample not valid
-                        if possible_sample.is_err() {eprintln!("."); continue;}
+                        if possible_sample.is_err() {
+                            //eprintln!(".");
+                            continue;}
                         // collect sample
-                        let sample = possible_sample.unwrap();
+                        let sample = possible_sample.unwrap().to_unit();
                         // check if this is the first line
                         if !got_first_line {
                             let _lab_start_time = Duration::from_micros(sample.time as u64);
                             // here we can dynamically infer the data format (YTF or YLD).
                             got_first_line = true
                         }
-                        let ystudio_time = (Instant::now() - start_time).as_millis() as f64;
-                        ylab_data.lock().unwrap().add(ystudio_time, sample);
-                        println!("{}", sample.to_csv_line());
+                        let ystudio_time = (Instant::now() - start_time).as_secs_f64();
+                        let hist_len = ylab_data.lock().unwrap().len();
+                        println!("{}: {} | {}", ystudio_time, sample.read[0], hist_len);
+                        for measure in sample.to_yld(Duration::from_millis(ystudio_time as u64)).iter() {
+                            ylab_data.lock().unwrap().add(ystudio_time, *measure);    
+                        }
+                        
+                        //println!("{}", sample.to_csv_line());
                     
                     }},
             State::Recording { path: _ } => {},
