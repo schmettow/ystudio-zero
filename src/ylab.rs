96d25249a8c72cb790ba2657a23206faa98aae62
schmettow <schmettow@web.de>
2023-11-02 19:37:06 +0100

    + extended Yld format + added Trait multilines for plotting + implemented trait fir Ytf and Yld

--- src/ylab.rs
+++ src/ylab.rs
@@ -5,7 +5,8 @@
 
 pub use std::fmt;
 pub use std::time::Instant;
-use std::path::PathBuf;
+pub use std::path::PathBuf;
+
 //use egui::epaint::tessellator::Path;
 //use serialport::SerialPort;
 
@@ -37,11 +38,6 @@ impl fmt::Display for YLabVersion {
 
 /// Optional list of serial port names
 pub type AvailablePorts = Option<Vec<String>>;
-/// Possible serial port
-//pub type PossPort = Option<Box<dyn SerialPort>>;
-/// Possible serial port
-//pub type PossReader = Option<Box<dyn std::io::BufRead>>;
-
 
 /// YLab State
 /// 
@@ -72,9 +68,9 @@ pub enum YLabCmd {
 
 pub mod ydata {
     pub use std::error::Error;
-    pub static CHAN_IDS: [&str; 8] = [ "y0", "y1", "y2", "y3",
-    "y4", "y5", "y6", "y7"];
+    use std::{time::Duration, collections::HashMap};
 
+    use egui::util::History;
     /// YLab Long Data
     /// 
     /// YLD keeps data one row per measure with 
@@ -85,12 +81,44 @@ pub mod ydata {
 
     #[derive(Copy, Clone, Debug)]
     pub struct Yld {
-        pub time: f64,
+        pub time: Duration,
         pub dev: i8,
-        pub value: f64,
-    }
+        pub sensory : i8,
+        pub probe: i8,
+        pub value: f64,}
+
+    pub type _YldBuf = Vec<Yld>;
+    pub use egui_plot::PlotPoints;
+    pub type MultiLines = Vec<Vec<[f64; 2]>>;
     
-    /// YLab transport format with width 8
+    /// Splitting a Yld History into a vector of point series
+    /// This is used to plot a bunch of lines in egui_plot
+    pub trait ToMultiLines {
+        fn split(&self) -> MultiLines;
+    }
+
+    impl ToMultiLines for History<Yld>{
+        fn split (&self) -> MultiLines {
+            let mut point_map: MultiLines = Vec::new();
+            for measure in self.iter() {
+                let time = measure.0;
+                let _dev = measure.1.dev as usize;
+                let _sensory = measure.1.sensory as usize;
+                let probe = measure.1.probe as usize;
+                let value = measure.1.value;
+                let point = [time, value];
+                let point_id = probe as usize;
+                match point_map.get_mut(point_id) {
+                    Some(points) => points.push(point),
+                    None => {point_map.insert(probe, vec!(point));},
+                }           
+            }
+            return point_map;
+        }
+    }
+
+
+    /// YLab transport format
     /// 
     /// YLabs send data with a time stamp,
     /// a device identifier and a vector of eight readings.
@@ -100,9 +128,12 @@ pub mod ydata {
     #[derive(Copy, Clone, Debug)]
     pub struct Ytf<const N: usize, T>  {
         pub dev: i8,
-        pub time: i64,
+        pub time: Duration,
         pub read: [T;N],
     }
+
+    /// YTF 8 implementation
+
 
     #[derive(Copy, Clone, Debug)]
     pub struct Ytf8 {
@@ -121,6 +152,25 @@ pub mod ydata {
     /// Result type for parsing CSV lines
     pub type FailableSample = Result<Ytf8, ParseError>;
     
+    impl ToMultiLines for Ytf8 {
+        fn split (&self) -> MultiLines {
+            let mut point_map: MultiLines = Vec::new();
+            let time = self.time as f64;
+            let dev = self.dev as usize;
+            let read = self.read;
+            for probe in 0..8 {
+                let value = read[probe];
+                let point = [time, value];
+                //let point_id = probe as usize;
+                match point_map.get_mut(probe) {
+                    Some(points) => points.push(point),
+                    None => {point_map.insert(probe, vec!(point));},
+                }           
+            }
+            return point_map;
+        }
+    }
+    
     /// Methods for YLab data samples
     impl Ytf8 {
         /// Create a new sample from a CSV line as String
@@ -134,7 +184,7 @@ pub mod ydata {
         /// Duration *since startup* is slightly stronger than Duration. 
         /// It's like every run has their own epoch.
         /// 
-        /// 
+        /// Collecting a sample fro a CSV line is a fallible operation.
         pub fn from_csv_line(line: &String) -> FailableSample {
             // splitting
             let cols: Vec<&str> = line.split(",").collect();
@@ -174,18 +224,24 @@ pub mod ydata {
             return out
         }
 
-        pub fn to_yld(&self, time: f64) -> Vec<Yld> {
+        pub fn to_yld(&self, time: Duration) -> Vec<Yld> {
             let mut out: Vec<Yld> = Vec::new();
-            //let mut pos: i8 = 0;
+            let mut probe: i8 = 0;
             for value in self.read.iter() {
-              //  pos += 1;
-                out.push(Yld{time, dev: self.dev, value: *value as f64});
+                out.push(Yld{time, dev: 0, sensory: 0, probe: self.dev, value: *value as f64});
+                probe += 1;
             };
             return out
         }
+        pub fn to_unit(mut self) -> Self {
+            const MAX: f64 = 32_768.0;
+            self.read = self.read.map(|r| r / MAX);
+            self
+        }
 
-        /*pub fn to_unit(&self) -> (){
-            let read_unit = self.read.map(|r| {r as f64/ (2^15) as f64 });
+/*        pub fn to_unit(&mut self) -> (){
+            const MAX: f64 = 32_768.0;
+            let read_unit = self.read.map(|r| {r/MAX});
             self.read = read_unit;
         }*/
     }
