83b819051f82feef192b1c69a8c7440be9f70187
schmettow <schmettow@web.de>
2023-11-11 00:20:52 +0100

    + rewrote entire state machine

--- src/yldest.rs
+++ src/yldest.rs
@@ -20,12 +20,19 @@ pub enum YldestCmd {
     Stop,
 }
 
+type LockedPath = Arc<Mutex<Option<PathBuf>>>;
+type LockedFile = Arc<Mutex<Option<File>>>;
+type LockedState = Arc<Mutex<YldestState>>;
 
+use std::fs::File;
 pub fn yldest_thread(
-    state: Arc<Mutex<YldestState>>,
+    state: LockedState,
     listen: mpsc::Receiver<YldestCmd>,
     incoming: mpsc::Receiver<Yld>,
 ) -> ! {
+    let locked_path: LockedPath = Arc::new(Mutex::new(None));
+    let locked_file: LockedFile = Arc::new(Mutex::new(None));
+    let mut write_buffer = String::new();
     loop {
         // getting the current state, incoming commands and incoming data
         let this_state = state.lock().unwrap().clone();
@@ -33,50 +40,42 @@ pub fn yldest_thread(
         let measure = incoming.try_recv().ok();
         // match the current state, command and data stream to do transitions
         match (this_state, this_cmd, measure) {
+            
             // start recording on command
-            (YldestState::Idle, Some(YldestCmd::New { path }), _) 
-                => {
-                let file = fs::File::create(path.clone());
-                match file {
-                    Ok(mut file) => {
-                        file.write_all(data::YLD_HEAD.as_bytes()).unwrap();
-                        *state.lock().unwrap() = YldestState::Recording {path};},
-                    Err(_) => {
-                        eprintln!("Could not create file");
-                    },
-                }
-            },
+            (YldestState::Idle, 
+            Some(YldestCmd::New { path }),
+            _) 
+            => {
+                *locked_path.lock().unwrap() = Some(path.clone());
+                *locked_file.lock().unwrap() = Some(fs::File::create(&path).unwrap());
+                *state.lock().unwrap() = YldestState::Recording {path: path.clone()};
+                println!("Recording to {}", path.to_str().unwrap());},
+            
             // do recording when new data arrived
-            (YldestState::Recording{path}, _, Some(measure))
-                => {
-                    let file = fs::File::open(&path);
-                    match file {
-                        Ok(mut file) => {
-                            file.write_all(measure.to_csv_line().as_bytes()).unwrap();},
-                        Err(_) => {
-                            eprintln!("Could not open file");
-                        },
-                    }
-                },
+            (YldestState::Recording{path}, 
+            _, 
+            Some(measure))
+            => {
+                write_buffer.push_str(&measure.to_csv_line());
+                if write_buffer.len() > 1000 {                    
+                    locked_file
+                        .lock().unwrap().as_ref().unwrap()
+                        .write_all(write_buffer.as_bytes()).unwrap();
+                    write_buffer.clear();
+                }},
+            
             // pause recording on command
-            (YldestState::Recording{path}, Some(YldestCmd::Pause), _) 
-                => {
-                    *state.lock().unwrap() = YldestState::Pausing{path};
-                },
+            (YldestState::Recording{path}, 
+            Some(YldestCmd::Pause), 
+            _) 
+            => {
+                *state.lock().unwrap() = YldestState::Pausing{path};
+            },
 
             // resume recording on command after pause
             (YldestState::Pausing{path}, Some(YldestCmd::Resume), _) 
-                => {
-                    let file = fs::File::open(&path);
-                    match file {
-                        Ok(mut file) => {
-                            file.write_all(data::YLD_HEAD.as_bytes()).unwrap();
-                            *state.lock().unwrap() = YldestState::Recording {path: path};},
-                        Err(_) => {
-                            eprintln!("Could not open file. Please close all other programs that might be using it.");
-                        },
-                    }
-                },
+                => {*state.lock().unwrap() = YldestState::Recording {path: path};},
+
             // stop recording on command
             (YldestState::Recording{path}, Some(YldestCmd::Stop), _) 
                 => {
